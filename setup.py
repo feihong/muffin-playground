7312d538d1ae177fe1e3e32f0dd943d2ebaec773
Feihong <feihong.hsu@gmail.com>
2016-08-11 11:19:02 -0500

    Add more fields in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
-from setuptools import setup
+import sys
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
 
 
 setup(
     name='muffin-playground',
-    py_modules=['muffin_playground'],
+    version='0.0.1',
+    license='Apache 2.0',
+    description=Path('README.rst').read_text(),
+    platforms=('Any',),
+    keywords='asyncio aiohttp muffin'.split(),
+    author='Feihong Hsu',
+    email='feihong.hsu@gmail.com',
+    url='https://github.com/feihong/muffin-playground',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=['muffin', 'plim'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
 )
